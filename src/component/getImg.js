f94089085a8340ecbf6c92517ce748bd75ad8a53
kamlesh sharma <ksharma6876@gmail.com>
2021-04-22 15:15:15 +0530

    add some responsive

--- src/component/getImg.js
+++ src/component/getImg.js
@@ -5,24 +5,32 @@ import '../css/getImg.css'
 const GetImg = () => {
     const [photo,setPhoto] = useState([])
     const [loading, setLoading] = useState(false)
-    const GetInputValue = () => {
+    const GetInputValue = (e) => {
         
         let input = document.getElementById('inputValue');
        
         let url = `https://api.unsplash.com/search/photos?page=1&query=${input.value}&${key}&per_page=20`;
         console.log(url)
-        if(input.value !== "") {
+        if(input.value !== "" ) {
         setLoading(true)
         fetch(url).then(response => response.json())
         .then(res => setPhoto(res.results),setLoading(false))
         .catch(err => console.log(err))
         }
         input.value = null;
-        
+    }
+
+    const onEnterKey = (e) => {
+        if(e.key === 'Enter') {
+            GetInputValue();
+        }
     }
 
     return(<>
-         <SearchBar getValue = {() => GetInputValue()}/>
+         <SearchBar 
+         getValue = {() => GetInputValue()} 
+         enterKey= {onEnterKey}
+         />
          {loading && <p>Loading...</p>}
          <div className="imgWrapper">
             {photo.map((imgs,index) =>(
--- src/component/search.js
+++ src/component/search.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../css/searchBar.css';
+
+
+
 export const SearchBar = (props) => {
     return(
         <div className="searchWrapper">
-         <input type="text" placeholder="Search for Image" id="inputValue"/>
-         <span onClick={props.getValue} className="btn">Search</span>
+         <input type="text" placeholder="Search for Image" id="inputValue" onKeyPress={props.enterKey}/>
+         <span onClick={props.getValue}  className="btn">Search</span>
         </div>
     )
 }
