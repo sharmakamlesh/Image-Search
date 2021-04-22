110e8528a21cfccca0db107f44079f1f046dbe60
kamlesh sharma <ksharma6876@gmail.com>
2021-04-22 21:33:00 +0530

    update img error not found

--- src/component/getImg.js
+++ src/component/getImg.js
@@ -1,46 +1,49 @@
-import React,{useState} from 'react';
-import {SearchBar} from './search';
-import {key} from '../constant'
-import '../css/getImg.css'
+import React, { useState } from "react";
+import { SearchBar } from "./search";
+import { key } from "../constant";
+import "../css/getImg.css";
 const GetImg = () => {
-    const [photo,setPhoto] = useState([])
-    const [loading, setLoading] = useState(false)
-    const GetInputValue = (e) => {
-        
-        let input = document.getElementById('inputValue');
-       
-        let url = `https://api.unsplash.com/search/photos?page=1&query=${input.value}&${key}&per_page=20`;
-        console.log(url)
-        if(input.value !== "" ) {
-        setLoading(true)
-        fetch(url).then(response => response.json())
-        .then(res => setPhoto(res.results),setLoading(false))
-        .catch(err => console.log(err))
-        }
-        input.value = null;
+  const [photo, setPhoto] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const GetInputValue = (e) => {
+    let input = document.getElementById("inputValue");
+    let url = `https://api.unsplash.com/search/photos?page=1&query=${input.value}&${key}`;
+    console.log(url);
+    if (input.value !== "") {
+      fetch(url)
+        .then((response) => response.json())
+        .then((res) => setPhoto(res.results))
+        .catch((err) => console.log(err));
     }
+    input.value = null;
+  };
 
-    const onEnterKey = (e) => {
-        if(e.key === 'Enter') {
-            GetInputValue();
-        }
+  const onEnterKey = (e) => {
+    if (e.key === "Enter") {
+      GetInputValue();
     }
+  };
 
-    return(<>
-         <SearchBar 
-         getValue = {() => GetInputValue()} 
-         enterKey= {onEnterKey}
-         />
-         {loading && <p>Loading...</p>}
-         <div className="imgWrapper">
-            {photo.map((imgs,index) =>(
-                <div className="imgCard" key={index}>
-                <img src={imgs.urls.small} alt="img" className="img"/>
-                </div>))}
-            </div>
-         </>
-    )
-}
-
+  return (
+    <>
+      <SearchBar getValue={() => GetInputValue()} enterKey={onEnterKey} />
+      {}
+      {loading ? <p>Loading...</p> : null}
+      <div className="imgWrapper">
+        {photo.length === 0 ? (
+          <div>
+            <h2>Not Found Any Images!!</h2>
+            <h3>Search for Photos </h3>
+          </div>
+        ) : null}
+        {photo.map((imgs, index) => (
+          <div className="imgCard" key={index}>
+            <img src={imgs.urls.small} alt="img" className="img" />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default GetImg;
